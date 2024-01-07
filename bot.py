758863df5dbc76ac2aa5aaac52001f0a35f32bcb
Andrey <139971881+Prockofi@users.noreply.github.com>
2024-01-07 14:37:19 +0300

    Оптимизация кода

--- bot.py
+++ bot.py
@@ -1,74 +1,83 @@
-import telebot, time
+import telebot
+from data import Data
 from telebot import types
 from get_marks import get
-from replace_data import rep, append, rm
-from schools_name import search
+from schools_names import search
 
-with open('token') as file:
-    token = file.read()
-bot = telebot.TeleBot(token)
+#Получаем токен бота из файла token
+with open('token') as token:
+    bot = telebot.TeleBot(token.read())
 
 #Отслеживаем команду /start
 @bot.message_handler(commands=["start"])
 def repeat_all_messages(message):
+    #Определяем кнопки [Помощь] [Дневник]
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Помощь")
-    btn2 = types.KeyboardButton("Дневник")
-    markup.add(btn1, btn2)
+    markup.add(types.KeyboardButton("Помощь"), types.KeyboardButton("Дневник"))
+    #Проходимся по всему списку пользователей и определяем зарегистрирован ли он
     with open('users', 'r', encoding='utf-8') as file:
         data = file.read().split('\n')
     for line in data:
-        if (str(message.chat.id) in line) and (len(line.split('#')) > 3):
-            bot.send_message(message.chat.id, 'Вы авторизованы! Напишите /help для получения информации', reply_markup=markup)
-            break
+        if str(message.chat.id) in line:
+            if len(line.split('#')) < 4:
+                bot.send_message(message.chat.id, 'Продолжайте регистрацию', reply_markup=markup)
+                break
+            if len(line.split('#')) > 3:
+                bot.send_message(message.chat.id, 'Вы авторизованы!', reply_markup=markup)
+                break
     else:
         bot.send_message(message.chat.id, 'Для начала работы, необходимо авторизоваться', reply_markup=markup)
-        append(str(message.chat.id), 'users')
-        bot.send_message(message.chat.id, 'Логин')
-    
+        Data.append(str(message.chat.id), 'users')
+        bot.send_message(message.chat.id, 'Введите логин')
+
+#Отслеживаем команду /help
 @bot.message_handler(commands=["help"])
 def repeat_all_messages(message):
     bot.send_message(message.chat.id, 'Доступные команды')
 
-
 #Берём данные для авторизации
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    if(message.text == "Помощь"):
-        bot.send_message(message.chat.id, 'Доступные команды')
-    elif (message.text == 'Дневник'):
-        bot.send_message(message.chat.id, 'Оценки')
+def repeat_all_messages(mess):
+    if mess.text == 'Помощь':
+        bot.send_message(mess.chat.id, 'Доступные команды')
+    elif mess.text == 'Дневник':
+        bot.send_message(mess.chat.id, 'Дневник')
     else:
+        #Url определён, только для Костромской области (нужно доработать)
+        url = 'https://netschool.eduportal44.ru/'
         with open('users', 'r', encoding='utf-8') as file:
             data = file.read().split('\n')
-        user = str(message.chat.id)
+        #Проходим по всем пользователям узнаём, колличество сохранённых данных и от этого выбираем этап регистрации
         for line in data:
-            if user == line:
-                line += '#' + message.text
-                bot.send_message(message.chat.id, 'Пароль')
-                break
-            if (user in line) and (len(line.split('#')) == 2):
-                line += '#' + message.text
-                bot.send_message(message.chat.id, 'Название школы')
-                break
-            if (user in line) and (len(line.split('#')) == 3):
-                send = bot.send_message(message.chat.id, 'Выполняется подключение...')
-                try:
-                    name_school = search(message.text, 'https://netschool.eduportal44.ru/')
-                    line += '#' + name_school
-                    rep(line, 'users')
-                    user_id, login, password, name_school = line.split('#')
-                    get(user_id, login, password, name_school, 'https://netschool.eduportal44.ru/')
-                    bot.edit_message_text('Подключение установленно! Теперь вы будете получать оповещения об изменениях в вашем дневнике', message.chat.id, send.id)
-                except:
-                    rm(line, 'users')
-                    bot.edit_message_text('Подключение не удалось! Попробуйте заново /start', message.chat.id, send.id)
-                break
-            if (user in line) and (len(line.split('#')) > 3):
-                bot.send_message(message.chat.id, 'Вы авторизованы! Напишите /help для получения информации')
-                break
+            length = len(line.split('#'))
+            if str(mess.chat.id) in line:
+                if length == 1:
+                    line += '#' + mess.text
+                    bot.send_message(mess.chat.id, 'Пароль')
+                    break
+                if length == 2:
+                    line += '#' + mess.text
+                    bot.send_message(mess.chat.id, 'Название школы')
+                    break
+                if length == 3:
+                    send = bot.send_message(mess.chat.id, 'Выполняется подключение...')
+                    try:
+                        name_school = search(mess.text, url)
+                        line += '#' + name_school
+                        Data.replace(line, 'users')
+                        user_id, login, password, name_school = line.split('#')
+                        get(user_id, login, password, name_school, url)
+                        bot.edit_message_text('Подключение установленно! Теперь вы будете получать оповещения об изменениях в вашем дневнике', mess.chat.id, send.id)
+                    except:
+                        data.remove(line, 'users')
+                        bot.edit_message_text('Подключение не удалось! Попробуйте заново /start', mess.chat.id, send.id)
+                    break
+                if length > 3:
+                    bot.send_message(mess.chat.id, 'Вы авторизованы! Напишите /help для получения информации')
+                    break
         else:
-            bot.send_message(message.chat.id, 'Для начала работы, необходимо авторизоваться - /start')
-        rep(line, 'users')
+            bot.send_message(mess.chat.id, 'Для начала работы, необходимо авторизоваться - /start')
+        Data.replace(line, 'users')
 
+#Начинаем работу бота
 bot.infinity_polling()
--- script.py
+++ script.py
@@ -1,35 +1,33 @@
-from get_marks import get
 #Функция get() принимает id, login, пароль, название школы, url-адрес школы
 #Возращает два списка: добавленные оценки и убранные
+from get_marks import get
+import telebot, time
 
-import time
-import telebot
-with open('token') as file:
-    token = file.read()
-bot = telebot.TeleBot(token)
+#Получаем токен бота из файла token
+with open('token') as token:
+    bot = telebot.TeleBot(token.read())
 
 #Проходимся вечным циклом по файлу с Users
 while True:
+    #Url определён, только для Костромской области (нужно доработать)
+    url = 'https://netschool.eduportal44.ru/'
     with open('users', 'r', encoding='utf-8') as file:
         data = file.read().split('\n')
-    #Работтаем через цикл с каждой записью
+    #Работаем через цикл с каждой записью, и проверяем, чтобы регистрация была завершена
     for line in data:
         if len(line.split('#')) == 4:
             user_id, login, password, name_school = line.split('#')
             #Получаем оценки, в случае ошибки возращаем пустые списки
             try:
-                new, old = get(user_id, login, password, name_school, 'https://netschool.eduportal44.ru/')
+                new, old = get(user_id, login, password, name_school, url)
             except:
-                new, old = [[], []]
-            str1, str2 = '', ''
-            for el in new:
-                str1 += '\n' + str(el)[:-2].replace("',", ' ').replace("'", '')
-            for el in old:
-                str2 += '\n' + str(el)[:-2].replace("',", ' ').replace("'", '')
-            if len(str1) > 10:
-                bot.send_message(user_id, f'Новые оценки:{str1}')
-            if len(str2) > 10:
-                bot.send_message(user_id, f'Эти оценки удалили:{str2}')
+                new, old = [], []
+            new = '\n' + '\n'.join(new)
+            old = '\n' + '\n'.join(old)
+            if len(new) > 10:
+                bot.send_message(user_id, f'Новые оценки:{new}')
+            if len(old) > 10:
+                bot.send_message(user_id, f'Эти оценки удалили:{old}')
     #1800 сек => 30 мин ожидания
     time.sleep(30)
         
